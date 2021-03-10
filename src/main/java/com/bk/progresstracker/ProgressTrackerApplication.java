b53036d2c822cf9228195ef5f746fff2f0157d52
William Korando <wkorando@gmail.com>
2021-03-10 09:16:43 -0600

    initial commit

---
+++ src/main/java/com/bk/progresstracker/ProgressTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.bk.progresstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bk/progresstracker/ProgressTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bk.progresstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
