31a3d7f74e62cb48f10f136099ab52e0766aab56
William Korando <wkorando@gmail.com>
2021-04-01 09:07:10 -0500

    Cleaning out generated docs branch

--- database/progress-tracker-ddl.sql
+++
@@ -1,3 +0,0 @@
-create sequence progress_data_id_generator start with 10 increment by 1;
-
-create table progress_data (id number(19,0) not null, json_data clob, lab_id varchar2(255), step_id varchar2(255), timestamp date, tracking_id varchar2(255), primary key (id));
